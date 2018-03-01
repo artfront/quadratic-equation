ef0c09a63b82ffa345d13991f5c348c33f180269
artfront <krivonosart19@gmail.com>
2018-03-01 11:08:19 +0300

    equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let roots = [];
+    const Arr = equation.split(' ').join('').replace('*x^2', ' ').replace('*x', ' ').split(' ');
+    const a = +Arr[0];
+    const b = +Arr[1];
+    const c = +Arr[2];
+    const d = Math.pow(b, 2) - 4 * a * c;
+    roots[0] = Math.round((-b + Math.sqrt(d)) / (2 * a));
+    roots[1] = Math.round((-b - Math.sqrt(d)) / (2 * a));
+     function compareNumeric(a, b) {
+         return a - b;
+     }
+     return roots.sort(compareNumeric);
 }
+
+
